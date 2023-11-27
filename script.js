e5cb199c9491544f502f7036b0a7e39925170204
islom <islom@gmail.com>
2023-11-27 18:28:00 +0500

    ssss

---
+++ script.js
@@ -0,0 +1,36 @@
+// class Human {
+//     constructor(obj) {
+//         this.name = obj.name
+//         this.age = obj.age
+//     }
+//     getInfo() {
+//         return `Your name is ${this.name}. Your age is ${this.age}`
+//     }
+
+// }
+// const Alisher = new Human({
+//     name: "Alisher",
+//     age: 25
+// })
+// const Sardor = new Human({
+//     name: "Sardor",
+//     age: 15
+// })
+// console.log(Alisher.getInfo());
+// console.log(Sardor.getInfo());
+
+// class Woman extends Human {
+//     constructor(props) {
+//         super(props)
+//         this.gender = 'female'
+//     }
+//     info() {
+//         return `Your name is ${this.name}. Your age is ${this.age}. Your gender is ${this.gender}`
+//     }
+// }
+// const Ziyoda = new Woman({
+//     name: "Ziyoda",
+//     age: 20,
+
+// })
+// console.log(Ziyoda.info());
